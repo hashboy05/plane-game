6cd96e48f93a641fec9c2b6c3b17983d1a74aaad
hashboy05 <AOHuang05@gmail.com>
2024-10-25 00:03:07 +0800

    added formations

--- Assets/Scripts/TargetSpawner.cs
+++ Assets/Scripts/TargetSpawner.cs
@@ -1,31 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class TargetSpawner : MonoBehaviour
 {
-    // [SerializeField] private Sprite[] targetSprite;
-    [SerializeField] private GameObject targetPrefab;
-    [SerializeField] private float cooldown;
-    private float timer;
+    [SerializeField] private GameObject[] targetPrefab;
+    // [SerializeField] private float cooldown;
+    // private float timer;
+    private int currentIndex = 0; // Track which prefab to spawn next
 
     void Update()
     {
-        timer -= Time.deltaTime;
-        if (timer < 0){
-            timer = cooldown;
-            GameObject newTarget = Instantiate(targetPrefab);
-            // Check for children in targetPrefab
-            if (newTarget.transform.childCount > 0)
+        // timer -= Time.deltaTime;
+        // if (timer < 0)
+        // {
+            // timer = cooldown;
+
+            // Make sure we have prefabs to spawn
+            if (targetPrefab.Length > 0)
             {
-                // Iterate through each child and add to the dictionary
-                foreach (Transform child in newTarget.transform)
-                {
-                    Damage.planes.Add(child.gameObject, 100); // Add each child to the dictionary
+                if (Damage.planes.Count==0){
+                    // Spawn the current prefab
+                    GameObject newTarget = Instantiate(targetPrefab[currentIndex]);
+
+                    if (newTarget.transform.childCount > 0)
+                    {
+                        foreach (Transform child in newTarget.transform)
+                        {
+                            Damage.planes.Add(child.gameObject, 100);
+                        }
+                    }
+
+                    newTarget.transform.position = new Vector2(0, transform.position.y);
+
+                    // Move to next prefab
+                    currentIndex++;
+
+                    // Reset index if we've reached the end of the array
+                    if (currentIndex >= targetPrefab.Length)
+                    {
+                        currentIndex = 0;
+                    }
                 }
             }
-            newTarget.transform.position = new Vector2(0, transform.position.y);
-        }
+        // }
     }
 }
